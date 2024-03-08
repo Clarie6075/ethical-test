9fc13330a44faa967e4db6664e603f6f439f0ee4
Clarie6075 <142280641+Clarie6075@users.noreply.github.com>
2024-03-08 19:45:52 +0800

    Update app.py

--- Ethics Test/app.py
+++ Ethics Test/app.py
@@ -2,14 +2,8 @@
 # coding: utf-8
 
 # In[ ]:
-
-
 from flask import Flask, request, render_template
-'''
-Flask is the web framework used to create the application.
-request is used to handle HTTP requests.
-render_template is used to render HTML templates.
-'''
+
 # Creates an instance of the Flask class, which is the WSGI application.
 app = Flask(__name__)
 
@@ -18,14 +12,8 @@ app = Flask(__name__)
 
 def index():  # handling requests to the root URL ("/").
 
-    if request.method =="POST":
-        rate = float(request.form.get("rate"))
-        print(rate)
-        # it renders the "index.html" template and passes a variable result with the value "ML model not read".
-        return(render_template("index.html",result = 90.2 + (-50.6*rate)))
-    else:
-
-        return(render_template("index.html",result = "waitting for exchange rate......"))
+    
+    return(render_template("index.html"))
 
 if __name__ =="__main__":
     app.run()
