1cefc7662e1f5765a1176f6b772552b741c8fe26
Clarie6075 <142280641+Clarie6075@users.noreply.github.com>
2024-03-15 19:06:14 +0800

    app.py

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask, request, render_template
-'''
-Flask is the web framework used to create the application.
-request is used to handle HTTP requests.
-render_template is used to render HTML templates.
-'''
+
 # Creates an instance of the Flask class, which is the WSGI application.
 app = Flask(__name__)
 
 # Decorates the index function to be called when the root URL ("/") is accessed with either a GET or POST request
 @app.route("/",methods = ["GET","POST"])
-
 def index():  # handling requests to the root URL ("/").
+    return(render_template("index.html"))
 
-    if request.method =="POST":
-        rate = float(request.form.get("rate"))
-        print(rate)
-        # it renders the "index.html" template and passes a variable result with the value "ML model not read".
-        return(render_template("index.html",result = 90.2 + (-50.6*rate)))
+
+@app.route("/main",methods = ["GET","POST"])
+def main():  
+    name = request.form.get("name")
+    return(render_template("main.html",name=name))
+
+@app.route("/ethical_test",methods = ["GET","POST"])
+def ethnical_test():  
+    return(render_template("ethical_test.html"))
+
+@app.route("/answer",methods = ["GET","POST"])
+def answer():
+    ans = request.form["options"]  
+    if ans =="true":
+        return(render_template("correct.html"))
     else:
+        return(render_template("wrong.html"))
 
-        return(render_template("index.html",result = "waitting for exchange rate......"))
+@app.route("/end",methods = ["GET","POST"])
+def end():  
+    return(render_template("end.html"))
 
 if __name__ =="__main__":
     app.run()
-
-
-# In[ ]:
-
-
-
-
